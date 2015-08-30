b64c3bfabd3e8fb0cdf0b5bfa7e809c99d14a06f
Andrew Chen <deweichen47@gmail.com>
2015-08-30 14:40:31 -0400

    overhaul  ui

--- js/controllers/TaskInputController.js
+++ js/controllers/TaskInputController.js
@@ -1,13 +1,12 @@
 taskTrackerApp.controller("TaskInputController", ["$scope", "taskListManager", 
 	function($scope, taskListManager) {
 
-		$scope.taskName = "My task ";
+		// $scope.taskName = "My task ";
 		// $scope.taskDurationSec = 10;
 		// $scope.taskDurationMin = 0;
 
 		$scope.addTask = function() {
       if ($scope.taskNameForm.$valid && $scope.taskDurationForm.$valid) {
-        console.log($scope.taskDurationSec);
   			var sec = ($scope.taskDurationMin || 0) * 60 + ($scope.taskDurationSec || 0);
   			taskListManager.addTask($scope.taskName, sec);
       }
